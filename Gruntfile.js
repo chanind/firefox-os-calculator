113fbb42381168a7492bfd11223164c2dafbd899
David Chanin <dchanin@quizlet.com>
2014-03-02 01:43:45 -0800

    fixing zip step

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,7 @@ module.exports = function(grunt) {
         },
         files: [
           {
-            src: 'dist/**'
+            expand: true, src: '**/*', cwd : "dist/"
           }
         ]
       }
